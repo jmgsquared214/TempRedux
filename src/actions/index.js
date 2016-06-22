b174a5e732384042476ccc606767115f9741072f
Jamie Greene <jmgsquared214@gmail.com>
2016-06-22 16:22:45 -0700

    adding code Lecture 57

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,3 @@
-
 import axios from 'axios';
 
 const API_KEY = '771795487359cf4c6ffa3bef7e20aa47';
@@ -7,11 +6,11 @@ const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KE
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
-  const url = `${ROOT_URL}&q=${city},us`;
-  const request = axios.get(url);
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
 
-  return {
-    type: 'FETCH_WEATHER'
-    payload: request
-  };
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
 }
