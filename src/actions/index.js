ddd002de1cea8d4fd303d68f7e8cc62926172844
Green <gk61gre@mgcc121355.ent.rt.csaa.com>
2016-07-28 16:09:27 -0700

    adding new react container and sparklines

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,7 +9,6 @@ export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
     const request = axios.get(url);
     //ajax request returns a promise
-console.log('Request:', request);
 
     return {
         type: FETCH_WEATHER,
