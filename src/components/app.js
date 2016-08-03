7d6c8e197f77eb990b61a4fb9d360d34ffccda8c
Green <gk61gre@mgcc121355.ent.rt.csaa.com>
2016-08-03 09:44:28 -0700

    fixed Stateless Componenet issue

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Component } from 'react';
+import {Component} from 'react';
 
 import SearchBar from '../containers/search_bar';
 import WeatherList from '../containers/weather_list';
@@ -8,8 +8,8 @@ export default class App extends Component {
   render() {
     return (
       <div>
-      <SearchBar />
-      <WeatherList />
+        <SearchBar />
+        <WeatherList />
       </div>
     );
   }
--- src/components/chart.js
+++ src/components/chart.js
@@ -1,13 +1,25 @@
+import _ from 'lodash';
 import React from 'react';
-import { Sparklines, SparklinesLine, SparklinesReferenceLines } from 'react-sparklines';
+import {Sparklines, SparklinesLine, SparklinesReferenceLine} from 'react-sparklines';
+
+function average(data) {
+  return _.round(_.sum(data) / data.length);
+}
+
+// Sparklines: pass the following props
+// data = an array of numbers
+// color = the sparkline color
+// height and width = of sparkline
+// units = symbol to be displayed next to the average
 
 export default (props) => {
   return (
     <div>
-      <Sparklines height={120} width={180} data={props.data}>
+    <Sparklines height={props.height} width={props.width} data={props.data}>
       <SparklinesLine color={props.color} />
-      <SparklinesReferenceLines type="avg" />
-      </Sparklines>
-      </div>
-  )
+      <SparklinesReferenceLine type="avg" />
+    </Sparklines>
+    <div>{average(props.data)} {props.units}</div>
+    </div>
+  );
 };
